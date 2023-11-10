f428ddf5fcc71898ed917020cbda71f16cce52ac
3sdd <56486103+3sdd@users.noreply.github.com>
2023-11-10 20:14:32 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,11 @@
 module.exports = {
 	root: true,
 	extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
+	overrides: [
+		{
+			rules: {
+				"vue/multi-word-component-names": "off",
+			},
+		},
+	],
 };
