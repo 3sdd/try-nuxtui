67106ff1a7baf80a757ca788b3106d193bda6d88
3sdd <56486103+3sdd@users.noreply.github.com>
2023-11-10 20:40:28 +0900

    badge試した

---
+++ components/elements/badge.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div>
+			<UBadge>バッジ</UBadge>
+			<UBadge label="バッジ" />
+		</div>
+		<div>
+			color:
+			<UBadge color="yellow" variant="solid" label="badge" />
+			<UBadge color="gray" variant="solid" label="badge" />
+			<UBadge color="black" variant="solid" label="badge" />
+			<UBadge color="black" variant="outline" label="badge" />
+		</div>
+		<div>
+			<UBadge size="lg">badge</UBadge>
+		</div>
+		<div>
+			<UBadge :ui="{ rounded: 'rounded-full' }">badge</UBadge>
+		</div>
+	</div>
+</template>
--- pages/elements.vue
+++ pages/elements.vue
@@ -4,6 +4,9 @@
 			<h2>accordion</h2>
 		</div> -->
 
+		<UDivider label="badge" />
+		<ElementsBadge />
+
 		<UDivider label="meter" />
 		<ElementsMeter />
 
